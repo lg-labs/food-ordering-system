f57893663b0999c84717c310149a4c1ffd6426f4
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-04-19 19:10:04 +0200

    add BaseEntity and AggregateRoot

---
+++ common/common-domain/src/main/java/com/labs/lg/food/ordering/system/domain/entity/AggregateRoot.java
@@ -0,0 +1,5 @@
+package com.labs.lg.food.ordering.system.domain.entity;
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+
+}
---
+++ common/common-domain/src/main/java/com/labs/lg/food/ordering/system/domain/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Domain classes used in different bounded contexts
+ * <p>
+ * These classes contain the common between different aggregates
+ * </p>
+ *
+ * @since 1.0.0-alpha
+ * @author Luis Fernando García Q.
+ * @version 1.0.0-alpha
+ */
+package com.labs.lg.food.ordering.system.domain;
