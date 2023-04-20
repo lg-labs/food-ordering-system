3ca850296ca9663f465d1e83ed3ccfea01178f74
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-04-21 01:03:27 +0200

    Add DomainException to common lib, so manage all exception in the domain

---
+++ common/common-domain/src/main/java/com/labs/lg/food/ordering/system/domain/exception/DomainException.java
@@ -0,0 +1,11 @@
+package com.labs.lg.food.ordering.system.domain.exception;
+
+public abstract class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+
+    public DomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
