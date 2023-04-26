1a297d7f2f408a3fd1e965a77b442e12bd5acbef
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-04-26 19:37:28 +0200

    Add OrderTrackCommandHandler
    Also, add message implementation as a base to a future into SAGA section

--- order-service/order-domain/order-application-service/src/main/java/com/labs/lg/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/labs/lg/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -2,15 +2,36 @@ package com.labs.lg.food.ordering.system.order.service.domain;
 
 import com.labs.lg.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.labs.lg.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.labs.lg.food.ordering.system.order.service.domain.entity.Order;
+import com.labs.lg.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.labs.lg.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.labs.lg.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import com.labs.lg.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
 public class OrderTrackCommandHandler {
 
+  private final OrderRepository orderRepository;
+  private final OrderDataMapper orderDataMapper;
 
-  public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery){
+  public OrderTrackCommandHandler(OrderRepository orderRepository, OrderDataMapper orderDataMapper) {
+    this.orderRepository = orderRepository;
+    this.orderDataMapper = orderDataMapper;
+  }
 
+  @Transactional(readOnly = true)
+  public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+    Optional<Order> orderResult = orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.orderTrackingId()));
+    if (orderResult.isEmpty()) {
+      log.warn("Could not find order with tracking id: {}", trackOrderQuery.orderTrackingId());
+      throw new OrderNotFoundException("Could not find order with tracking id: " + trackOrderQuery.orderTrackingId());
+    }
+    return orderDataMapper.orderToTrackOrderResponse(orderResult.get());
   }
 }
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/labs/lg/food/ordering/system/order/service/domain/exception/OrderNotFoundException.java
@@ -0,0 +1,10 @@
+package com.labs.lg.food.ordering.system.order.service.domain.exception;
+
+import com.labs.lg.food.ordering.system.domain.exception.DomainException;
+
+public class OrderNotFoundException extends DomainException {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
