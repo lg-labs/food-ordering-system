0ef928b43e95f3d59f19f1e1fd8d7eb4f7e3da5e
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-04-29 23:53:54 +0200

    Add builder to TrackOrderQuery dto

--- order-service/order-domain/order-application-service/src/main/java/com/labs/lg/food/ordering/system/order/service/domain/dto/track/TrackOrderQuery.java
+++ order-service/order-domain/order-application-service/src/main/java/com/labs/lg/food/ordering/system/order/service/domain/dto/track/TrackOrderQuery.java
@@ -1,7 +1,10 @@
 package com.labs.lg.food.ordering.system.order.service.domain.dto.track;
 
+import lombok.Builder;
+
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
+@Builder
 public record TrackOrderQuery(@NotNull UUID orderTrackingId) {
 }
