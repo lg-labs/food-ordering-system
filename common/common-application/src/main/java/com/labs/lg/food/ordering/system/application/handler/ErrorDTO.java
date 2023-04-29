1c27e5bf815cbd3d671884425d356035b345dbb9
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-04-30 01:02:14 +0200

    finished implement application layer
    Implement a REST API with its error handler(domain server)

---
+++ common/common-application/src/main/java/com/labs/lg/food/ordering/system/application/handler/ErrorDTO.java
@@ -0,0 +1,6 @@
+package com.labs.lg.food.ordering.system.application.handler;
+
+import lombok.Builder;
+
+@Builder
+public record ErrorDTO(String code, String message) { }
---
+++ common/common-application/src/main/java/com/labs/lg/food/ordering/system/application/handler/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.labs.lg.food.ordering.system.application.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+/**
+ * <h1>Caught Global Exception Handler</h1>
+ * <h1>Catch Server errors </h1>
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ResponseBody
+  @ExceptionHandler(value = {Exception.class})
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ErrorDTO handleException(Exception exception) {
+    log.error(exception.getMessage(), exception);
+    return ErrorDTO.builder()
+        .code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+        .message("Unexpected error!")
+        .build();
+  }
+
+  @ResponseBody
+  @ExceptionHandler(value = {ValidationException.class})
+  @ResponseStatus(BAD_REQUEST)
+  public ErrorDTO handleException(ValidationException validationException) {
+    ErrorDTO errorDTO;
+    if (validationException instanceof ConstraintViolationException constraintViolationException) {
+      final String validations = extractViolationsFromException(constraintViolationException);
+      log.error(validations, validationException);
+      errorDTO = ErrorDTO.builder()
+          .code(BAD_REQUEST.getReasonPhrase())
+          .message(validations)
+          .build();
+      return errorDTO;
+    }
+    final String exceptionMessage = validationException.getMessage();
+    log.error(exceptionMessage, validationException);
+    errorDTO = ErrorDTO.builder()
+        .code(BAD_REQUEST.getReasonPhrase())
+        .message(exceptionMessage)
+        .build();
+    return errorDTO;
+  }
+
+  private String extractViolationsFromException(ConstraintViolationException validationException) {
+    return validationException.getConstraintViolations()
+        .stream()
+        .map(ConstraintViolation::getMessage)
+        .collect(Collectors.joining("--"));
+  }
+}
