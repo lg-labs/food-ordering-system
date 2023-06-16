20d612ca150d679cf1daa1490be7d660d9cc38a0
Luis F. García Q <105936384+luisferlabs@users.noreply.github.com>
2023-06-16 18:05:45 +0200

    feat(order-service): Completed order domain using SAGA flows
    Change import test dependency

--- order-service/order-domain/order-domain-core/src/main/java/com/labs/lg/food/ordering/system/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/labs/lg/food/ordering/system/order/service/domain/entity/Order.java
@@ -50,16 +50,42 @@ public class Order extends AggregateRoot<OrderId> {
     }
 
     public void pay() {
-// default implementation ignored
+        if (orderStatus != OrderStatus.PENDING) {
+            throw new OrderDomainException("Order is not in correct state for pay operation!");
+        }
+        orderStatus = OrderStatus.PAID;
     }
 
-    public void approve() {// default implementation ignored
+    public void approve() {
+        if(orderStatus != OrderStatus.PAID) {
+            throw new OrderDomainException("Order is not in correct state for approve operation!");
+        }
+        orderStatus = OrderStatus.APPROVED;
     }
 
-    public void initCancel(List<String> failureMessages) {// default implementation ignored
+    public void initCancel(List<String> failureMessages) {
+        if (orderStatus != OrderStatus.PAID) {
+            throw new OrderDomainException("Order is not in correct state for initCancel operation!");
+        }
+        orderStatus = OrderStatus.CANCELLING;
+        updateFailureMessages(failureMessages);
     }
 
-    public void cancel(List<String> failureMessages) {// default implementation ignored
+    public void cancel(List<String> failureMessages) {
+        if (!(orderStatus == OrderStatus.CANCELLING || orderStatus == OrderStatus.PENDING)) {
+            throw new OrderDomainException("Order is not in correct state for cancel operation!");
+        }
+        orderStatus = OrderStatus.CANCELLED;
+        updateFailureMessages(failureMessages);
+    }
+
+    private void updateFailureMessages(List<String> failureMessages) {
+        if (this.failureMessages != null && failureMessages != null) {
+            this.failureMessages.addAll(failureMessages.stream().filter(message -> !message.isEmpty()).toList());
+        }
+        if (this.failureMessages == null) {
+            this.failureMessages = failureMessages;
+        }
     }
 
     /**
