638f8482a55118e43acbcd6c25e4f947337efac4
Luis Fernando García Q <105936384+lglabs@users.noreply.github.com>
2024-11-02 22:33:46 +0100

    [LG-73] - add acceptance test module for customer as base

---
+++ customer-service/customer-acceptance-test/src/test/java/com/labs/lg/food/ordering/system/customer/service/MyStepdefs.java
@@ -0,0 +1,19 @@
+package com.labs.lg.food.ordering.system.customer.service;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("a repository template")
+    public void aRepositoryTemplate() {
+    }
+
+    @When("blank is created")
+    public void blankIsCreated() {
+    }
+
+    @Then("the blank will be created using the repository template")
+    public void theBlankWillBeCreatedUsingTheRepositoryTemplate() {
+    }
+}
